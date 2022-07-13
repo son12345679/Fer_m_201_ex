305632cba0380f73fb83d3ffed006083baaa9653
son <sonvnmse150320@fpt.edu.vn>
2022-07-13 16:29:07 +0700

    Intro. to Redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react"
 import { BrowserRouter } from "react-router-dom"
-import { Provider } from "react-redux"
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
+
 import store from "./redux/store"
 
 import "./App.css";
 import Main from "./components/MainComponent";
 
 function App() {
-	return (
+	return 
 		<Provider store={store}>
-			<BrowserRouter>
-				<div>
-					<Main />
-				</div>
-			</BrowserRouter>
-		</Provider>
-	)
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+
 }
 
 export default App;
