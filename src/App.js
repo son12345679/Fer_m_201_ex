e7aa84aaf828d28d7f35dedbce0312bf740f2ff6
son <sonvnmse150320@fpt.edu.vn>
2022-07-13 13:57:55 +0700

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import DishDetail from "./components/DishDetailComponent.js";
 import { DISHES } from './shared/dishes.js';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
