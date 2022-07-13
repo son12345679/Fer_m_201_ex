fc107d644f57a09c8ed5f1588006bb57b33edc9c
son <sonvnmse150320@fpt.edu.vn>
2022-07-13 14:04:17 +0700

    lab5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, {Component} from "react";
+import DishDetail from "./components/DishDetailComponent.js";
 import { DISHES } from './shared/dishes.js';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
 
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES
-    };
+  };
+}
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <DishDetail dishes={this.state.dishes} />
+      </div>
+    );
   }
 }
-<Menu dishes={this.state.dishes} />
+
+
 export default App;
