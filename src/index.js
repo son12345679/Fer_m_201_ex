5a32e45b99148fe963ad4ef814d0cbcadedb7ed9
son <sonvnmse150320@fpt.edu.vn>
2022-07-19 11:58:38 +0700

    final lab8

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
@@ -7,11 +7,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,12 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import { Dishes } from './dishes'
-import { Comments } from './comments'
-import { Promotions } from './promotions'
-import { Leaders } from './leaders'
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 import { createForms } from 'react-redux-form';
 import { InitialFeedback } from './forms';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -19,6 +19,7 @@ export const ConfigureStore = () => {
                 feedback: InitialFeedback
             })
         }),
+
         applyMiddleware(thunk, logger)
     );
 
